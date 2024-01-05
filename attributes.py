32204e4efce29a0090c6b8743ba94a687b624b75
Juan <j.tejeda46@yahoo.com>
2024-01-04 20:35:49 -0600

    added methods to class

--- attributes.py
+++ attributes.py
@@ -7,12 +7,28 @@ class Car:
 		self.color = color
 		self.gas_level = 100 
 
+	def drive(self, miles):
+		self.gas_level -= (miles // 4)
+		print(f'{self.gas_level}% Fuel Remaining')
+
+	def gas_station(self, amount):
+		if self.gas_level == 100:
+			print('Tank is Full')
+			return
+		while self.gas_level != 100 and amount > 0:
+			self.gas_level += 1
+			amount -= 1
+			print(f'Gas level {self.gas_level}% remaining ${amount}')
+		print(f'Tank is at {self.gas_level}% Thank you for stopping at our Fuel Station!')
+		if amount > 0:
+			print(f'${amount} refunded')
+
 
 dream_car = Car('Porsche', '911 GT3 RS', 'Red')
 juans_car = Car('Volkswagen', 'Jetta', 'White')
 sherleys_car = Car('Toyota', 'Corolla', 'Dark Blue')
 
-print(dream_car.gas_level)
-dream_car.gas_level -= 15
-print(dream_car.gas_level)
 
+
+dream_car.drive(100)
+dream_car.gas_station(20)
