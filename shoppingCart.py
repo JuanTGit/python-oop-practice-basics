1e3c45a0a81e8c6045eed04feefdfb27c44b5510
Juan <j.tejeda46@yahoo.com>
2024-01-05 16:35:24 -0600

    Shopping Cart OOP

--- shoppingCart.py
+++ shoppingCart.py
@@ -2,29 +2,71 @@
 
 class Cart():
     
-    def __init__(self, cart=[]):
-        self.cart = cart
+    def __init__(self):
+        self.my_cart = {}
+        self.total = 0
     
-    def add_to_cart(self, item):
-        self.cart.append(item)
+    def add_to_cart(self, item, price):
+        self.my_cart[item] = price
+        self.total += price
 
     def remove_from_cart(self, item):
-        self.cart.remove(item)
+        if not item in self.my_cart:
+            print(f'{item} does not exist in your cart.')
+            return
+        else:
+            self.total -= self.my_cart[item]
+            self.my_cart.pop(item)
     
     def show_cart(self):
-        for item in self.cart:
-            print(f'{item}')
+        if not self.my_cart:
+            print('Your cart is empty!')
+            return
+        print('General Store')
+        print('=============')
+        for item, value in self.my_cart.items():
+            print(f'{item}: ${value}')
+        print('=============')
+        print(f'Total: ${self.total}')
+        print('_____________')
 
 
-buggie = Cart()
+def run():
+    cart = Cart()
+    while True:
+        ask = input('What would you like to do? add/remove/show/quit? ').lower()
+        if ask == 'quit':
+            break
+        elif ask == 'add':
+            while True:
+                item = input('What would you like to add? ').lower()
+                if item == 'quit':
+                    break
+                price = int(input('How much does it cost? '))
+                cart.add_to_cart(item, price)
 
-buggie.add_to_cart('Juice')
-buggie.add_to_cart('Bacon')
-buggie.add_to_cart('Water')
-buggie.add_to_cart('Eggs')
+        elif ask == 'remove':
+            item = input('What would you like to remove? ')
+            cart.remove_from_cart(item)
 
-buggie.show_cart()
+        elif ask == 'show':
+            cart.show_cart()
 
-buggie.remove_from_cart('Eggs')
+        else:
+            print('Invalid response. Please try again.')
 
-buggie.show_cart()
+run()
+
+
+cart = {
+        'tea': 8,
+        'coffee': 3,
+        'eggs': 5,
+        'chips': 2
+    }
+
+# print(cart['tea'])
+# cart.pop('tea')
+# print(cart)
+# for i, v in cart.items():
+#     print(i, v)
